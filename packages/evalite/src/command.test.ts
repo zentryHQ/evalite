0878c2ed7f05e30e04c30660114f62920a3bfdc7
Matt Pocock <mattpocockvoice@gmail.com>
2025-01-01 09:39:04 +0000

    CI fix

--- packages/evalite/src/command.test.ts
+++ packages/evalite/src/command.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vitest } from "vitest";
-import { createProgram } from "./command";
+import { createProgram } from "./command.js";
 
 describe("createCommand", () => {
   it("evalite without path", async () => {
